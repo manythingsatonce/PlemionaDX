03bb2327d9a1d8e50ee1eeb866f570f622b56059
manythingsatonce <damian_patora@hotmail.com>
2021-08-22 21:53:39 +0200

    Add script for instant build.

--- src/js/components/skippingBuilding.js
+++ src/js/components/skippingBuilding.js
@@ -1,7 +1,48 @@
+import { SKIPPING_BUILDING_INTERVAL } from '../constants';
+
 class SkippingBuilding {
 	constructor() {}
 
-	init() {}
+	/**
+	 * Initiates a search for opportunities to accelerate the construction of buildings.
+	 *
+	 * @returns {Void}
+	 */
+	init() {
+		this._searchInstantBuild();
+	}
+
+	/**
+	 * Looks for the possibility of completing the construction.
+	 *
+	 * @private
+	 * @returns {Void}
+	 */
+	_searchInstantBuild() {
+		setInterval(() => {
+			const [button] = document.getElementsByClassName('btn-instant-free');
+
+			if (button) {
+				const { display } = button.style;
+
+				if (display !== 'none') {
+					this._instantBuild(button);
+				}
+			}
+		}, SKIPPING_BUILDING_INTERVAL);
+	}
+
+	/**
+	 * Completes construction immediately.
+	 *
+	 * @param {Element} button - Button to be pressed to complete construction.
+	 *
+	 * @private
+	 * @returns {Void}
+	 */
+	_instantBuild(button) {
+		button.click();
+	}
 }
 
 export default SkippingBuilding;
--- src/js/constants.js
+++ src/js/constants.js
@@ -4,3 +4,8 @@
 export const COMPONENTS_TO_LOAD = {
 	MAIN: 'main',
 };
+
+/**
+ * The delay between looking for a button in {@link https://developer.mozilla.org/pl/docs/Web/API/WindowOrWorkerGlobalScope/setInterval milliseconds}.
+ */
+export const SKIPPING_BUILDING_INTERVAL = 1000;
