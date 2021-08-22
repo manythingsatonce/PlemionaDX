8e10ec6f3036195278d7e0772a1a43f154420c06
manythingsatonce <damian_patora@hotmail.com>
2021-08-22 22:32:49 +0200

    Extraction of data about the building to be accelerated has been added.

--- src/js/components/skippingBuilding.js
+++ src/js/components/skippingBuilding.js
@@ -1,7 +1,13 @@
 import { SKIPPING_BUILDING_INTERVAL } from '../constants';
 
 class SkippingBuilding {
-	constructor() {}
+	constructor() {
+		this._observedConstruction = {
+			building: '',
+			level: 0,
+			accelerated: true,
+		};
+	}
 
 	/**
 	 * Initiates a search for opportunities to accelerate the construction of buildings.
@@ -25,6 +31,10 @@ class SkippingBuilding {
 			if (button) {
 				const { display } = button.style;
 
+				if (this._observedConstruction.accelerated) {
+					this._getBuildingDetails(button);
+				}
+
 				if (display !== 'none') {
 					this._instantBuild(button);
 				}
@@ -42,6 +52,28 @@ class SkippingBuilding {
 	 */
 	_instantBuild(button) {
 		button.click();
+
+		this._observedConstruction.accelerated = true;
+	}
+
+	/**
+	 * Extracts data about the building, the construction of which is to be accelerated.
+	 *
+	 * @param {Element} button - Button to be pressed to complete construction.
+	 *
+	 * @private
+	 * @returns {Void}
+	 */
+	_getBuildingDetails(button) {
+		const parent = button.closest('.lit');
+		const [column] = parent.querySelectorAll('.lit-item');
+		const data = column.innerText;
+		const [building] = data.split('\n');
+		const level = parseFloat(data.replace(/^\D+/g, ''));
+
+		this._observedConstruction.accelerated = false;
+		this._observedConstruction.building = building;
+		this._observedConstruction.level = level;
 	}
 }
 
